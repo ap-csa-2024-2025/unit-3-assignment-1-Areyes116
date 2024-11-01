8a6ed0130ff47d86502f7cac96b2b54807e7e134
Areyes116 <adrian.reyes2006@icloud.com>
2024-11-01 11:13:34 -0700

    Arise

--- Main.java
+++ Main.java
@@ -1,37 +1,4 @@
-class Rectangle
-{
-    private int length;
-    private int width;
 
-    public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
-    }
-
-    public int getLength()
-    {
-        return length;
-    }
-
-    public int getWidth()
-    {
-        return width;
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass())
-        {
-            return false;
-        }
-        Rectangle rectangle = (Rectangle) obj;
-        return length == rectangle.length && width == rectangle.width;
-    }
-}
 
 public class Main
 {
